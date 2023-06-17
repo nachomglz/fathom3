1faa6f682ff0413e71cee4409c1a7ee5045f217c
nachomglz <nachopad3@gmail.com>
2023-06-17 02:03:45 +0200

    Added crud for expense lists

---
+++ backend/src/routers/expenseList.router.ts
@@ -0,0 +1,16 @@
+import { PrismaClient } from "@prisma/client";
+import { FastifyInstance } from "fastify";
+import { createExpenseList, getExpenseLists, getExpenseList, updateExpenseList, deleteExpenseList } from "../controllers/expenseList.controller";
+
+const prisma = new PrismaClient();
+
+const expenseListRouter = (fastify: FastifyInstance, options: any, done: () => void) => {
+    fastify.post('/', createExpenseList);
+    fastify.get('/', getExpenseLists);
+    fastify.get('/:id', getExpenseList);
+    fastify.put('/:id', updateExpenseList);
+    fastify.delete('/:id', deleteExpenseList);
+    done()
+}
+
+export default expenseListRouter
