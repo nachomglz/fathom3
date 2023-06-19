5321e9e4bc863cb1f33c5260533c07725d6eee41
nachomglz <nachopad3@gmail.com>
2023-06-19 13:32:08 +0200

    added login page with mocked flows

--- frontend/src/pages/Login/Login.tsx
+++ frontend/src/pages/Login/Login.tsx
@@ -1,10 +1,34 @@
-import React from 'react'
-import { LoginWrapper } from './style'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { LoginWrapper, LoginForm, FormButtons } from './style'
+import { Input, Button } from '../../components'
+import { Type as ButtonType } from '../../components/Button'
+import { validator } from '../../utils'
 
 const Login: React.FC = () => {
+  const navigate = useNavigate()
+
+  const [email, setEmail] = useState<string>()
+  const [password, setPassword] = useState<string>()
+
+  const handleLogin = () => {
+    // Validate email
+    if (email && validator.isEmail(email)) {
+      localStorage.setItem('email', email)
+      navigate("/")
+    }
+  }
+
   return (
     <LoginWrapper>
-      Login form
+      <LoginForm onSubmit={(e) => e.preventDefault()}>
+        <Input label="Email" placeholder="Introduce tu email" onChange={(e) => setEmail(e.target.value)} />
+        <Input label="Password" placeholder="Introduce tu password" type="password" onChange={(e) => setPassword(e.target.value)} />
+        <FormButtons>
+          <Button type={ButtonType.NORMAL} onClick={handleLogin}>Acceder</Button>
+          <Button type={ButtonType.INFO}>Crear cuenta</Button>
+        </FormButtons>
+      </LoginForm>
     </LoginWrapper>
   )
 }
--- frontend/src/pages/Login/style.ts
+++ frontend/src/pages/Login/style.ts
@@ -1,4 +1,18 @@
 import styled from 'styled-components'
 
 export const LoginWrapper = styled.div`
+  max-width: 300px;
+`
+
+export const LoginForm = styled.form`
+`
+
+export const FormButtons = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: 1rem;
+  button {
+    width: 100%;
+  }
 `
